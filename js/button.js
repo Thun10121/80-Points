5704a9acefb6cffc82409dc24cadc3f79b7337cf
Thunder10121 <Thundershadow10121@gmail.com>
2022-02-25 22:01:56 -0800

    beta 1.2.8.1
    all basic front page ui finalized
    disregarding instructions

--- js/button.js
+++ js/button.js
@@ -7,7 +7,7 @@ let frontButtonDIV = document.getElementById("front-buttons-div");
 let frontPageElement = document.getElementById("front-page-elements");
 let frontGame = document.getElementById("front-game-content");
 let backButton = document.getElementById("front-game-button-back");
-
+let joinBackButton = document.getElementById("front-button-back");
 
 let insStatus = false;
 
@@ -39,15 +39,16 @@ function instructions() {
 function join() {
     createButton.style.display = "none";
     joinButton.style.display = "none";
-    backButton.style.display = "flex";
+    joinBackButton.style.display = "flex";
     frontButtonDIV.innerHTML = `
-    <input id="front-join-input" class="front-button" type="number" placeholder="Room Code"></input>
-    <button id="front-button-enter" class="front-button" onclick="enterCode()">
-        <p id="front-button-enter-text" class="front-button-text">ENTER</p>
-    </button>
-    <button id="front-button-back" class="front-button" onclick="back()">
-        <p id="front-button-back-text" class="front-button-text">BACK</p>
-    </button>
+    <div id="front-button-input-background-border">
+        <input id="front-join-input" class="front-button" type="number"  maxlength="10" placeholder="Room Code"></input>
+    </div>
+    <div id="front-button-enter-background-border">
+        <button id="front-button-enter" class="front-button" onclick="enterCode()">
+            <p id="front-button-enter-text" class="front-button-text">ENTER</p>
+        </button>
+    </div>
     `;
 }
 
@@ -61,13 +62,18 @@ function create(){
 
 function back(){
     frontButtonDIV.innerHTML = `
-    <button id="front-button-create" class="front-button" onclick="create()">
+    <div id="front-button-background-border">
+        <button id="front-button-create" class="front-button" onclick="create()">
         <p id="front-button-create-text" class="front-button-text">CREATE</p>
     </button>
-    <button id="front-button-join" class="front-button" onclick="join()">
-        <p id="front-button-join-text" class="front-button-text">JOIN</p>
-    </button>
+    </div>
+    <div id="front-button-background-border">
+        <button id="front-button-join" class="front-button" onclick="join()">
+            <p id="front-button-join-text" class="front-button-text">JOIN</p>
+        </button>
+    </div>
     `;
+    joinBackButton.style.display = "none";
     backButton.style.display = "none";
     frontGame.style.display = "none";
     frontPageElement.style.display = "flex";
