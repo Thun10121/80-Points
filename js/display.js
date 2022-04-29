dbc28a253166d87866d2c28600616da42372f65e
hbl-2006 <83988914+hbl-2006@users.noreply.github.com>
2022-04-29 10:18:08 -0700

    1.23: full star system added

--- js/display.js
+++ js/display.js
@@ -17,15 +17,26 @@ function createCard(cardInputted, zhuSuit, zhuNumber) {
     let cardSuit = cardInputted.substring(0, 1);
     let cardNumber = cardInputted.substring(1, cardInputted.length);
     let suitColor = "red";
-    let zhuStarDisplay = "hide";
+    let zhuHollowStarDisplay = "hide", zhuStarDisplay = "hide", zhuStarDoubleDisplay = "hide", zhuStarTripleDisplay = "hide";
     if (cardSuit == "♣" || cardSuit == "♠") {
         suitColor = "black";
         borderColor = "blackBK";
     } else {
         borderColor = "redBK";
     }
-    if(cardSuit == zhuSuit || cardNumber == zhuNumber || cardNumber == "Joker"){
-        zhuStarDisplay = "show";
+    if (cardSuit == zhuSuit || cardNumber == zhuNumber || cardNumber == "Joker") {
+        if (cardSuit == zhuSuit && cardNumber == zhuNumber) {
+            zhuStarDisplay = "show";
+            zhuStarDoubleDisplay = "show";
+        }else if(cardNumber == zhuNumber){
+            zhuStarDisplay = "show";
+        }else if (cardNumber == "Joker") {
+            zhuStarDisplay = "show";
+            zhuStarDoubleDisplay = "show";
+            zhuStarTripleDisplay = "show";
+        }else{
+            zhuHollowStarDisplay = "show";
+        }
     }
     let cardsHTML = `
     <div id="card-border">
@@ -56,7 +67,64 @@ function createCard(cardInputted, zhuSuit, zhuNumber) {
                         transform="translate(-7.55 -0.99)" />
                 </svg>
                 <div id="card-center-suit" class="${suitColor}">${cardSuit}</div>
+                <svg id="zhuHollowStar" class = "${zhuHollowStarDisplay}" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#00ad11"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
                 <svg id="zhuStar" class="${zhuStarDisplay}" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px"
+                    viewBox="0 0 24 24" width="24px" fill="#00ad11">
+                    <linearGradient id="gold" x1="36.44" y1="51.18" x2="86.38" y2="30.25"
+                        gradientUnits="userSpaceOnUse">
+                        <stop offset="0.34" stop-color="#f0d750" />
+                        <stop offset="0.56" stop-color="#e7cd3b" />
+                        <stop offset="0.72" stop-color="#bc8c1f" />
+                        <stop offset="0.82" stop-color="#d9ad15" />
+                        <stop offset="1" stop-color="#fce186" />
+                    </linearGradient>
+                    <g>
+                        <rect fill="none" height="24" width="24" />
+                    </g>
+                    <g>
+                        <g>
+                            <g>
+                                <g>
+                                    <path
+                                        d="M12,22C6.49,22,2,17.51,2,12S6.49,2,12,2s10,4.04,10,9c0,3.31-2.69,6-6,6h-1.77c-0.28,0-0.5,0.22-0.5,0.5 c0,0.12,0.05,0.23,0.13,0.33c0.41,0.47,0.64,1.06,0.64,1.67C14.5,20.88,13.38,22,12,22z M12,4c-4.41,0-8,3.59-8,8s3.59,8,8,8 c0.28,0,0.5-0.22,0.5-0.5c0-0.16-0.08-0.28-0.14-0.35c-0.41-0.46-0.63-1.05-0.63-1.65c0-1.38,1.12-2.5,2.5-2.5H16 c2.21,0,4-1.79,4-4C20,7.14,16.41,4,12,4z" />
+                                    <circle cx="6.5" cy="11.5" r="1.5" />
+                                    <circle cx="9.5" cy="7.5" r="1.5" />
+                                    <circle cx="14.5" cy="7.5" r="1.5" />
+                                    <circle cx="17.5" cy="11.5" r="1.5" />
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+                <svg id="zhuStarDouble" class="${zhuStarDoubleDisplay}" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px"
+                    viewBox="0 0 24 24" width="24px" fill="#00ad11">
+                    <linearGradient id="gold" x1="36.44" y1="51.18" x2="86.38" y2="30.25"
+                        gradientUnits="userSpaceOnUse">
+                        <stop offset="0.34" stop-color="#f0d750" />
+                        <stop offset="0.56" stop-color="#e7cd3b" />
+                        <stop offset="0.72" stop-color="#bc8c1f" />
+                        <stop offset="0.82" stop-color="#d9ad15" />
+                        <stop offset="1" stop-color="#fce186" />
+                    </linearGradient>
+                    <g>
+                        <rect fill="none" height="24" width="24" />
+                    </g>
+                    <g>
+                        <g>
+                            <g>
+                                <g>
+                                    <path
+                                        d="M12,22C6.49,22,2,17.51,2,12S6.49,2,12,2s10,4.04,10,9c0,3.31-2.69,6-6,6h-1.77c-0.28,0-0.5,0.22-0.5,0.5 c0,0.12,0.05,0.23,0.13,0.33c0.41,0.47,0.64,1.06,0.64,1.67C14.5,20.88,13.38,22,12,22z M12,4c-4.41,0-8,3.59-8,8s3.59,8,8,8 c0.28,0,0.5-0.22,0.5-0.5c0-0.16-0.08-0.28-0.14-0.35c-0.41-0.46-0.63-1.05-0.63-1.65c0-1.38,1.12-2.5,2.5-2.5H16 c2.21,0,4-1.79,4-4C20,7.14,16.41,4,12,4z" />
+                                    <circle cx="6.5" cy="11.5" r="1.5" />
+                                    <circle cx="9.5" cy="7.5" r="1.5" />
+                                    <circle cx="14.5" cy="7.5" r="1.5" />
+                                    <circle cx="17.5" cy="11.5" r="1.5" />
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+                <svg id="zhuStarTriple" class="${zhuStarTripleDisplay}" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px"
                     viewBox="0 0 24 24" width="24px" fill="#00ad11">
                     <linearGradient id="gold" x1="36.44" y1="51.18" x2="86.38" y2="30.25"
                         gradientUnits="userSpaceOnUse">
